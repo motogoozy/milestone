33343e5dfa58ea1564b8c8d3a763aa3ad12fdecf
kspayne93 <kyle@investavante.com>
2019-01-16 17:29:44 -0700

    added Link

--- src/components/HeaderMain/HeaderMain.js
+++ src/components/HeaderMain/HeaderMain.js
@@ -1,23 +1,25 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './HeaderMain.css';
+import stoneIcon from '../../assets/milestoneIcon2.png';
 
 class HeaderMain extends Component {
    render() {
-      const stoneIcon = 'http://cdn.onlinewebfonts.com/svg/img_498163.png';
 
       return (
          <div className='header-nav'>
             <div className='navbar'>
-               <div className='brand-container'>
-                  <img src={stoneIcon} alt="" className='icon'/>
-                  <p className='brand' >Milestone</p>
-               </div>
-                  <div className='logout-button-container'>
+               <Link to='/' style={{textDecoration: 'none'}} >
+                  <div className='brand-container'>
+                     <img src={stoneIcon} alt="" className='icon'/>
+                     <p className='brand' >Milestone</p>
+                  </div>
+               </Link>
+               <div className='logout-button-container'>
                      <Link to='/' className='logout-button' >
                         Logout
                      </Link>
-                  </div>
+               </div>
             </div>
          </div>
       );
