e3c94af8c766902221825c000ee6e68457650b5c
kspayne93 <kyle@investavante.com>
2019-01-16 17:26:49 -0700

    added Login, Register, AddMilestone, and EditMilestone routes

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,18 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage/LandingPage';
 import Dashboard from './components/Dashboard/Dashboard';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+import AddMilestone from './components/AddMilestone/AddMilestone';
+import EditMilestone from './components/EditMilestone/EditMilestone';
 
 export default (
    <Switch>
       <Route component={LandingPage} exact path='/' />
       <Route component={Dashboard} path='/dashboard' />
+      <Route component={Login} path='/login' />
+      <Route component={Register} path='/register' />
+      <Route component={AddMilestone} path='/add' />
+      <Route component={EditMilestone} path='/edit' />
    </Switch>
 )
