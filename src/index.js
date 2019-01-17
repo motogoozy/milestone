74e6445c65c3e2c2bf2eb434dda69019e44aac2b
kspayne93 <kspayne93@gmail.com>
2019-01-16 17:26:29 -0700

    added HashRouter and provided store to app

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {HashRouter} from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './ducks/store';
 
 ReactDOM.render(
 
-<HashRouter>
-	<App />
-</HashRouter>
+<Provider store={store}>
+	<HashRouter>
+		<App />
+	</HashRouter>
+</Provider>
 
 , document.getElementById('root'));
 
