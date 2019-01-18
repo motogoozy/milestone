867462c672c600ad9efd691b86c1e931e630ecae
kspayne93 <kspayne93@gmail.com>
2019-01-17 23:09:14 -0700

    updated styling

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,21 +1,12 @@
 import React, { Component } from 'react';
-import BackgroundImage from '../../assets/background.png';
 import LandingPageHeader from './LandingPageHeader/LandingPageHeader';
 import './LandingPage.css';
 
-var selectionStyle = {
-   backgroundImage: `url(${BackgroundImage})`,
-   backgroundSize: 'contain',
-   height: '1400px',
-   width: '100vw',
-   backgroundRepeat: 'no-repeat',
-   position: 'fixed'
-}
 
 class LandingPage extends Component {
    render() {
       return (
-         <div className='background' style={selectionStyle} >
+         <div className='background' >
          < LandingPageHeader />
             <p>A simple way to remember life's special moments.</p>
          </div>
