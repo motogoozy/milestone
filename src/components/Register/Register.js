c0a5442ed9e255d58ddb53363afd7ef224c08f4e
kspayne93 <kyle@investavante.com>
2019-01-17 20:09:38 -0700

    added functionality to inputs and buttons to register

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 import BackgroundImage from '../../assets/background.png';
 import stoneIcon from '../../assets/milestoneIcon2.png';
 import './Register.css';
-
-
+import axios from 'axios';
+import Swal from 'sweetalert2';
 
 
 var selectionStyle = {
@@ -17,6 +17,24 @@ var selectionStyle = {
 }
 
 class Register extends Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         username: '',
+         password: '',
+         profile_pic: ''
+      }
+   }
+
+   async register() {
+      const { username, password, profile_pic } = this.state;
+      const res = await axios.post(`/auth/register`, { username: username, password: password, profile_pic: profile_pic })
+      if (res.data.loggedIn) {
+         this.props.history.push('/dashboard')
+      }
+   }
+
+
    render() {
       return (
          <div className='main' style={selectionStyle}>
@@ -33,10 +51,13 @@ class Register extends Component {
 
             <div className='register-menu'>
                <h2 className='register-text'>Sign Up</h2>
-               <input type="text" placeholder='Create a Username' className='register-input' />
-               <input type="text" placeholder='Create a Password' className='register-input' />
-               <input type="text" placeholder='Upload a Profile Picture' className='register-input' />
-               <button className='register-input' >Submit</button>
+               <input onChange={ (e) => this.setState({username: e.target.value}) } type="text" placeholder='Create a Username' className='register-input' />
+               <input onChange={ (e) => this.setState({password: e.target.value}) } type="text" placeholder='Create a Password' className='register-input' />
+               <input onChange={ (e) => this.setState({profile_pic: e.target.value}) } type="text" placeholder='Upload a Profile Picture' className='register-input' />
+               <div>
+                  <button onClick={() => this.props.history.push('/')} >Back</button>
+                  <button onClick={() => this.register()} className='register-input' >Submit</button>
+               </div>
             </div>
          </div>
       );
