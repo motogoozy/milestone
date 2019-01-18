2dc45801449e292b7a04896b296d4786c235f2b7
kspayne93 <kspayne93@gmail.com>
2019-01-17 23:07:32 -0700

    updated styling

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,20 +1,13 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import BackgroundImage from '../../assets/background.png';
 import stoneIcon from '../../assets/milestoneIcon2.png';
 import './Register.css';
 import axios from 'axios';
 import Swal from 'sweetalert2';
+import TextField from '@material-ui/core/TextField';
 
 
-var selectionStyle = {
-   backgroundImage: `url(${BackgroundImage})`,
-   backgroundSize: 'contain',
-   height: '1400px',
-   width: '100vw',
-   backgroundRepeat: 'no-repeat',
-   position: 'fixed'
-}
+
 
 class Register extends Component {
    constructor(props) {
@@ -30,14 +23,27 @@ class Register extends Component {
       const { username, password, profile_pic } = this.state;
       const res = await axios.post(`/auth/register`, { username: username, password: password, profile_pic: profile_pic })
       if (res.data.loggedIn) {
+         await Swal(
+            await Swal(
+               'Welcome!',
+               'You have successfully created an account.',
+               'success'
+            )
+         )
          this.props.history.push('/dashboard')
+      } else {
+         await Swal({
+            type: 'error',
+            title: 'Oops!',
+            text: res.data.message
+         })
       }
    }
 
 
    render() {
       return (
-         <div className='main' style={selectionStyle}>
+         <div className='main'>
             <div className='navbar'>
                <Link to='/' style={{textDecoration: 'none'}}>
                   <div className='brand-container'>
@@ -51,12 +57,29 @@ class Register extends Component {
 
             <div className='register-menu'>
                <h2 className='register-text'>Sign Up</h2>
-               <input onChange={ (e) => this.setState({username: e.target.value}) } type="text" placeholder='Create a Username' className='register-input' />
-               <input onChange={ (e) => this.setState({password: e.target.value}) } type="text" placeholder='Create a Password' className='register-input' />
-               <input onChange={ (e) => this.setState({profile_pic: e.target.value}) } type="text" placeholder='Upload a Profile Picture' className='register-input' />
+               <TextField
+                  label="Create a Username"
+                  value={this.state.username}
+                  onChange={ (e) => this.setState({username: e.target.value}) }
+                  margin="normal"
+               />
+               <TextField
+                  id="standard-password-input"
+                  label="Create a Password"
+                  value={this.state.password}
+                  onChange={ (e) => this.setState({password: e.target.value}) }
+                  margin="normal"
+                  type="password"
+               />
+               <TextField
+                  label="Profile Picture URL"
+                  value={this.state.profile_pic}
+                  onChange={ (e) => this.setState({profile_pic: e.target.value}) }
+                  margin="normal"
+               />
                <div>
-                  <button onClick={() => this.props.history.push('/')} >Back</button>
-                  <button onClick={() => this.register()} className='register-input' >Submit</button>
+                  <button onClick={() => this.props.history.push('/')} className='button' >Back</button>
+                  <button onClick={() => this.register()} className='button' >Submit</button>
                </div>
             </div>
          </div>
