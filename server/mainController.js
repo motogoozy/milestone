0648f6f540d0a7b2f97481a7d12cc7e023da5476
kspayne93 <kyle@investavante.com>
2019-01-19 09:19:23 -0700

    added editMilestone method (not working yet) and getOne method

--- server/mainController.js
+++ server/mainController.js
@@ -10,30 +10,49 @@ module.exports = {
       const { title, description, date, location, img } = req.body;
       const db = req.app.get('db');
 
-      const newMilestone = await db.add_milestone(
-         { title, description, date, location, img, author_id: req.session.user.user_id }
-      )
+      const newMilestone = await db.add_milestone({ title, description, date, location, img, author_id: req.session.user.user_id })
       res.status(200).send({message: 'Milestone Added'})
    },
 
-   updateMilestone: async(req, res) => {
+   editMilestone: async(req, res) => {
+      const { milestone_id, title, description, date, location, img } = req.body;
+      const db = req.app.get('db');
 
+      const oneMilestone = await db.get_one_milestone( {milestone_id} )
+      const updatedMilestone = {
+         milestone_id, 
+         title: title || oneMilestone.title,
+         description: description || oneMilestone.description,
+         date: date || oneMilestone.date,
+         location: location || oneMilestone.location,
+         img: img || oneMilestone.img
+      }
+
+      const editMilestone = await db.edit_milestone( updatedMilestone )
+      res.status(200).send({message: 'Milestone updated'})
    },
 
    deleteMilestone: async (req, res) => {
       const { milestone_id } = req.params;
       const db = req.app.get('db');
 
-      const deleted = await db.delete_milestone({milestone_id})
+      const deleted = await db.delete_milestone({ milestone_id })
       res.status(200).send({message: 'Milestone deleted'})
    }, 
 
    userData: async(req, res) => {
       if(req.session.user) {
          res.status(200).send(req.session.user)
-         
       } else {
          res.status(401).send('Please log in')
       }
+   },
+
+   getOne: async(req, res) => {
+      const { milestone_id } = req.params;
+      const db = req.app.get('db');
+
+      const one = await db.get_one_milestone({milestone_id})
+      res.status(200).send(one)
    }
 }
