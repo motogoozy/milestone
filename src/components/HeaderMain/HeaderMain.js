f875f8ee5238b4adf947479f59d01e870469bdca
kspayne93 <kspayne93@gmail.com>
2019-01-19 22:22:40 -0700

    created user object on redux state, subscribed header to reducer file

--- src/components/HeaderMain/HeaderMain.js
+++ src/components/HeaderMain/HeaderMain.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { withRouter} from 'react-router-dom';
+import { getUserData } from '../../ducks/reducer';
+import axios from 'axios';
 import './HeaderMain.css';
 import stoneIcon from '../../assets/milestoneIcon2.png';
-import axios from 'axios';
 import Swal from 'sweetalert2';
 
 class HeaderMain extends Component {
+
+   async componentDidMount() {
+      const res = await axios.get('/api/userData')
+      this.props.getUserData(res.data)
+   }
 
    async logout() {
       await axios.get('/auth/logout')
       const toast = Swal.mixin({
          toast: true,
-         position: 'top-end',
+         position: 'center',
          showConfirmButton: false,
          timer: 3000
       });
@@ -25,6 +32,7 @@ class HeaderMain extends Component {
    }
 
    render() {
+      const { username, profile_pic } = this.props.user;
 
       return (
          <div className='header-nav'>
@@ -35,13 +43,28 @@ class HeaderMain extends Component {
                      <p className='brand' >Milestone</p>
                   </div>
                </Link>
-               <div className='logout-button-container'>
-                  <p onClick={() => this.logout()} className='logout-button'>Logout</p>  
+         
+               <div className='profile-container' >
+                  <div className='logout-button-container'>
+                     <p onClick={() => this.logout()} className='logout-button'>Logout</p>
+                  </div>
+                  <div className='user-info'>
+                     {
+                        profile_pic ? (
+                           <img src={profile_pic} className='profile-pic' ></img>
+                        ) : <img src='https://t3.ftcdn.net/jpg/00/64/67/80/240_F_64678017_zUpiZFjj04cnLri7oADnyMH0XBYyQghG.jpg' className='profile-pic' ></img>
+                     }
+                  </div>
                </div>
+               
             </div>
          </div>
       );
    }
 }
 
-export default withRouter(HeaderMain);
+
+const mapStateToProps = (reduxState) => reduxState;
+
+
+export default connect(mapStateToProps, {getUserData})(withRouter(HeaderMain));
