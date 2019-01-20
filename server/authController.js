1e44fc9d24907f72f68168a03548d7acf7199580
kspayne93 <kyle@investavante.com>
2019-01-19 22:19:40 -0700

    added more user info onto session

--- server/authController.js
+++ server/authController.js
@@ -7,14 +7,14 @@ module.exports = {
 
       const userArr = await db.find_user({ username });
       if (userArr.length >=1) {
-         return res.status(200).send({message: 'Username already taken. Please enter a different username.', loggedIn: false})
+         return res.status(200).send({ message: 'Username already taken. Please enter a different username.', loggedIn: false })
       }
 
       const salt = bcrypt.genSaltSync(10);
       const hash = bcrypt.hashSync(password, salt);
 
       const newUserArr = await db.create_user({ username, hash, profile_pic });
-      req.session.user = {user_id: newUserArr[0].user_id};
+      req.session.user = { user_id: newUserArr[0].user_id, username: newUserArr[0].username, profile_pic: newUserArr[0].profile_pic };
       res.status(200).send({ message: 'logged in', userData: req.session.user, loggedIn: true })
    },
 
@@ -24,13 +24,13 @@ module.exports = {
 
       const userArr = await db.find_user({ username: username });
       if(!userArr[0]) {
-         return res.status(200).send({message: 'User not found.'})
+         return res.status(200).send({ message: 'User not found.' })
       }
       const result = bcrypt.compareSync(password, userArr[0].hash)
       if (!result) { //if result is falsy, passwords don't match up
-         return res.status(200).send({message: 'Incorrect password.'})
+         return res.status(200).send({ message: 'Incorrect password.' })
       }
-      req.session.user = {user_id: userArr[0].user_id };
+      req.session.user = { user_id: userArr[0].user_id, username: userArr[0].username, profile_pic: userArr[0].profile_pic };
       res.status(200).send({ message: 'logged in', userData: req.session.user, loggedIn: true })
    }, 
 
