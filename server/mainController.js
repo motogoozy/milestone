d2911fec0e2927dba974aeb02b6b23a40647cac2
kspayne93 <kspayne93@gmail.com>
2019-01-19 22:19:52 -0700

    beautified code

--- server/mainController.js
+++ server/mainController.js
@@ -2,7 +2,7 @@ module.exports = {
    getAll: async (req, res) => {
       const db = req.app.get('db');
 
-      const allMilestones = await db.get_all_milestones({user_id: req.session.user.user_id})
+      const allMilestones = await db.get_all_milestones({ user_id: req.session.user.user_id })
       res.status(200).send(allMilestones);
    },
 
@@ -11,14 +11,14 @@ module.exports = {
       const db = req.app.get('db');
 
       const newMilestone = await db.add_milestone({ title, description, date, location, img, author_id: req.session.user.user_id })
-      res.status(200).send({message: 'Milestone Added'})
+      res.status(200).send({ message: 'Milestone Added' })
    },
 
    editMilestone: async(req, res) => {
       const { milestone_id, title, description, date, location, img } = req.body;
       const db = req.app.get('db');
 
-      const oneMilestone = await db.get_one_milestone( {milestone_id} )
+      const oneMilestone = await db.get_one_milestone({ milestone_id })
       const updatedMilestone = {
          milestone_id, 
          title: title || oneMilestone.title,
@@ -29,7 +29,7 @@ module.exports = {
       }
 
       const editMilestone = await db.edit_milestone( updatedMilestone )
-      res.status(200).send({message: 'Milestone updated'})
+      res.status(200).send({ message: 'Milestone updated' })
    },
 
    deleteMilestone: async (req, res) => {
@@ -52,7 +52,7 @@ module.exports = {
       const { milestone_id } = req.params;
       const db = req.app.get('db');
 
-      const one = await db.get_one_milestone({milestone_id})
+      const one = await db.get_one_milestone({ milestone_id })
       res.status(200).send(one)
    }
 }
