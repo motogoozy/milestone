69f3c20698aa435a8d174e45c8c40ee6cc56aadd
kspayne93 <kyle@investavante.com>
2019-01-19 22:20:04 -0700

    beautified code

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+
 import LandingPage from './components/LandingPage/LandingPage';
 import Dashboard from './components/Dashboard/Dashboard';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
 import AddMilestone from './components/AddMilestone/AddMilestone';
 import EditMilestone from './components/EditMilestone/EditMilestone';
+
+
 
 export default (
    <Switch>
