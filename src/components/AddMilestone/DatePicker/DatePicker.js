948853f706e97378bc27dee2f58952fa2d64b255
kspayne93 <kyle@investavante.com>
2019-01-21 14:15:33 -0700

    created component

---
+++ src/components/AddMilestone/DatePicker/DatePicker.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+// import moment from 'moment';
+
+
+const styles = theme => ({
+   container: {
+      display: 'flex',
+      flexWrap: 'wrap',
+   },
+   textField: {
+
+   },
+   });
+
+
+   function DatePicker(props) {
+      const { classes } = props;
+
+      return (
+         <form className={classes.container} noValidate>
+            <TextField
+            id="date"
+            label="Date"
+            type="date"
+            className={classes.textField}
+            onChange={(e)=> props.handleDateChange(e.target.value)}
+            InputLabelProps={{
+               shrink: true,
+            }}
+            />
+         </form>
+      );
+   }
+
+   DatePicker.propTypes = {
+      classes: PropTypes.object.isRequired,
+   };
+
+export default withStyles(styles)(DatePicker);
