cf625ce51bde030d315fb87bfa063d2e81d7f486
kspayne93 <kyle@investavante.com>
2019-01-23 09:21:07 -0700

    added new db call to get milestones by ascending order (oldest first)

---
+++ db/get_all_milestones_asc.sql
@@ -0,0 +1,6 @@
+SELECT id, title, description, date, location, img, author_id, user_id, username, profile_pic
+FROM milestones
+   JOIN users
+   ON users.user_id = milestones.author_id
+WHERE users.user_id = ${user_id}
+ORDER BY id ASC;
