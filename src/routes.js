02bc98fc67d7fb82720347299df78d56acd86086
kspayne93 <kspayne93@gmail.com>
2019-01-23 10:11:26 -0700

    deleted unnecessary route

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-
 import LandingPage from './components/LandingPage/LandingPage';
 import Dashboard from './components/Dashboard/Dashboard';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
 import AddMilestone from './components/AddMilestone/AddMilestone';
-import EditMilestone from './components/EditMilestone/EditMilestone';
 
 
 
@@ -17,6 +15,5 @@ export default (
       <Route component={Login} path='/login' />
       <Route component={Register} path='/register' />
       <Route component={AddMilestone} path='/add' />
-      <Route component={EditMilestone} path='/edit' />
    </Switch>
 )
