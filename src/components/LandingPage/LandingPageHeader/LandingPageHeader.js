81e1cd4dff7ec1bdc8896c23e6f585692efb5b9d
kspayne93 <kyle@investavante.com>
2019-01-24 12:23:26 -0700

    made mobile responsive

--- src/components/LandingPage/LandingPageHeader/LandingPageHeader.js
+++ src/components/LandingPage/LandingPageHeader/LandingPageHeader.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import LandingNavButtons from './LandingNavButtons/LandingNavButtons';
 import './LandingPageHeader.scss';
-import stoneIcon from '../../../assets/milestoneIcon2.png'
-
+import stoneIcon from '../../../assets/milestoneIcon2.png';
+import hamburgerMenuIcon from '../../../assets/hamburger-menu-icon.png';
+import { Link } from 'react-router-dom';
 
 
 class LandingPageHeader extends Component {
+   constructor() {
+      super();
+      this.state = {
+         showMenu: false,
+      }
+   }
 
    render() {
 
@@ -16,8 +23,21 @@ class LandingPageHeader extends Component {
                <p className='brand' >Milestone</p>
             </div>
             
-            <div>
+            <div className='login-register-buttons'>
                < LandingNavButtons />
+            </div>
+            
+            <div className='menu-icon-container'>
+               <img src={hamburgerMenuIcon} alt="" className='menu-icon' onClick={ (e) => this.setState({showMenu: !this.state.showMenu })} />
+            </div>
+
+            <div className={ this.state.showMenu ? 'side-menu slide' : 'side-menu'}>
+               <Link to='/register' style={{textDecoration: 'none'}}>
+                  <p className='side-menu-button'>Sign Up</p>
+               </Link>
+               <Link to='/login' style={{textDecoration: 'none'}}>
+                  <p className='side-menu-button'>Login</p>
+               </Link>
             </div>
          </div>
       );
